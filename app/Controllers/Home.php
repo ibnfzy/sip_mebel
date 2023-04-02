f807370bff6f56a04a9c212f4c78dabb780980ed
jultdev <andikaraeng32@gmail.com>
2023-04-02 14:39:27 +0800

    controller frontend

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,134 @@
 
 namespace App\Controllers;
 
+use App\Models\ItemModel;
+
 class Home extends BaseController
 {
+    protected $cart;
+    protected $itemModel;
+    protected $db;
+
+    public function __construct()
+    {
+        $this->db = \Config\Database::connect();
+        $this->cart = \Config\Services::cart();
+        $this->itemModel = new ItemModel();
+    }
+
     public function index()
     {
-        return view('welcome_message');
+        return view('web/home');
+    }
+
+    public function item()
+    {
+        return view('web/item');
+    }
+
+    public function item_detail($id)
+    {
+        return view('web/item_detail', [
+            'data' => $this->itemModel->find($id)
+        ]);
+    }
+
+    public function cart()
+    {
+        return view('web/cart');
+    }
+
+    public function add_item()
+    {
+        $get = $this->itemModel->find($this->request->getPost('id'));
+
+        $this->cart->insert([
+            'id' => $get['id_item'],
+            'qty' => 1,
+            'price' => $get['harga_item'],
+            'name' => $get['nama_item'],
+            'gambar' => $get['gambar_item'],
+            'stok' => $get['stok_item']
+        ]);
+
+        return $this->response->setJSON(['msg' => 'Item berhasil masuk ke keranjang']);
+    }
+
+    public function remove_item($rowId)
+    {
+        $this->cart->remove($rowId);
+
+        return redirect()->to(base_url('cart'));
+    }
+
+    public function clear_cart()
+    {
+        $destroy = new \CodeIgniterCart\Config\Services;
+
+        $destroy->cart()->destroy();
+        $_SESSION['diskon'] = 0;
+        $_SESSION['id_diskon'] = null;
+
+        return redirect()->to(base_url('cart'));
+    }
+
+    public function update_cart()
+    {
+        $rowid = $this->request->getPost('rowid');
+        $qty = $this->request->getPost('qtybutton');
+        $stok = $this->request->getPost('stok');
+        $status = true;
+
+        for ($i = 1; $i <= count($this->cart->contents()); $i++) {
+            if ($qty[$i] > $stok[$i]) {
+                $status = false;
+                break;
+            }
+
+            $this->cart->update([
+                'rowid' => $rowid[$i],
+                'qty' => $qty[$i]
+            ]);
+        }
+
+        if ($status == false) {
+            return redirect()->to(base_url('cart'))->with('type-status', 'error')
+                ->with('message', 'Kuantitas item melebihi stok');
+        }
+
+        return redirect()->to(base_url('cart'))->with('type-status', 'success')
+            ->with('message', 'Berhasil diperbaruhi');
+    }
+
+    public function diskon_get()
+    {
+        $session = session();
+        if (isset($_SESSION['id_user'])) {
+            $get = $this->db->table('voucheruser')
+                ->where('id_user', $_SESSION['id_user'])
+                ->where('id_user_voucher', $this->request->getPost('id'))
+                ->get()
+                ->getResultArray();
+
+            if ($get) {
+                $session->set([
+                    'diskon' => $get[0]['potongan'],
+                    'id_diskon' => $get[0]['id_user_voucher']
+                ]);
+                return $this->response->setJSON([
+                    'msg' => 'Kode Diskon tersedia!',
+                    'id' => $get[0]['id_user_voucher'],
+                    'diskon' => $get[0]['potongan']
+                ]);
+            } else {
+                return $this->response->setJSON([
+                    'msg' => 'ID tidak ditemukan'
+                ]);
+            }
+        } else {
+            return $this->response->setJSON([
+                'msg' => 'Anda belum login!, silahkan login terlebih dahulu!'
+            ]);
+        }
     }
 }
---
+++ app/Views/web/base.php
@@ -0,0 +1,148 @@
+<!--
+author: W3layouts
+author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>🏘 Meubel Shop</title>
+  <!-- for-mobile-apps -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="keywords" content="Grocery Store Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+  <script type="application/x-javascript">
+  addEventListener("load", function() {
+    setTimeout(hideURLbar, 0);
+  }, false);
+
+  function hideURLbar() {
+    window.scrollTo(0, 1);
+  }
+  </script>
+  <!-- //for-mobile-apps -->
+  <link href="<?= base_url(''); ?>/assets/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+  <link href="<?= base_url(''); ?>/assets/css/style.css" rel="stylesheet" type="text/css" media="all" />
+  <!-- font-awesome icons -->
+  <link href="<?= base_url(''); ?>/assets/css/font-awesome.css" rel="stylesheet" type="text/css" media="all" />
+  <!-- //font-awesome icons -->
+  <script src="<?= base_url(''); ?>/swal/dist/sweetalert2.min.js"></script>
+  <link rel="stylesheet" href="<?= base_url(''); ?>/swal/dist/sweetalert2.min.css">
+  <link rel="stylesheet" href="<?= base_url('/'); ?>/toastr/build/toastr.min.css">
+  <!-- js -->
+  <script src="<?= base_url(''); ?>/assets/js/jquery-1.11.1.min.js"></script>
+  <!-- //js -->
+  <link href='//fonts.googleapis.com/css?family=Ubuntu:400,300,300italic,400italic,500,500italic,700,700italic'
+    rel='stylesheet' type='text/css'>
+  <link
+    href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic'
+    rel='stylesheet' type='text/css'>
+  <!-- start-smoth-scrolling -->
+  <script type="text/javascript" src="<?= base_url(''); ?>/assets/js/move-top.js"></script>
+  <script type="text/javascript" src="<?= base_url(''); ?>/assets/js/easing.js"></script>
+  <script type="text/javascript">
+  jQuery(document).ready(function($) {
+    $(".scroll").click(function(event) {
+      event.preventDefault();
+      $('html,body').animate({
+        scrollTop: $(this.hash).offset().top
+      }, 1000);
+    });
+  });
+  </script>
+  <!-- start-smoth-scrolling -->
+</head>
+
+<body>
+  <?= $this->include('web/layout/navbar'); ?>
+  <!-- banner -->
+
+  <!-- //newsletter -->
+  <!-- footer -->
+  <?= $this->include('web/layout/footer'); ?>
+  <!-- //footer -->
+  <!-- Bootstrap Core JavaScript -->
+  <script src="<?= base_url(''); ?>/assets/js/bootstrap.min.js"></script>
+  <script src="<?= base_url('/'); ?>/toastr/build/toastr.min.js"></script>
+  <script>
+  $(document).ready(function() {
+    $(".dropdown").hover(
+      function() {
+        $('.dropdown-menu', this).stop(true, true).slideDown("fast");
+        $(this).toggleClass('open');
+      },
+      function() {
+        $('.dropdown-menu', this).stop(true, true).slideUp("fast");
+        $(this).toggleClass('open');
+      }
+    );
+  });
+  </script>
+  <!-- here stars scrolling icon -->
+  <script type="text/javascript">
+  $(document).ready(function() {
+    /*
+    	var defaults = {
+    	containerID: 'toTop', // fading element id
+    	containerHoverID: 'toTopHover', // fading element hover id
+    	scrollSpeed: 1200,
+    	easingType: 'linear' 
+    	};
+    */
+
+    $().UItoTop({
+      easingType: 'easeOutQuart'
+    });
+
+  });
+  </script>
+  <!-- //here ends scrolling icon -->
+  <script src="<?= base_url(''); ?>/assets/js/minicart.js"></script>
+  <script>
+  paypal.minicart.render();
+
+  paypal.minicart.cart.on('checkout', function(evt) {
+    var items = this.items(),
+      len = items.length,
+      total = 0,
+      i;
+
+    // Count the number of each item in the cart
+    for (i = 0; i < len; i++) {
+      total += items[i].get('quantity');
+    }
+
+    if (total < 3) {
+      alert('The minimum order quantity is 3. Please add more to your shopping cart before checking out');
+      evt.preventDefault();
+    }
+  });
+  </script>
+
+  <?= $this->renderSection('script'); ?>
+
+  <script>
+  toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "showDuration": "600",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
+  </script>
+
+</body>
+
+</html>
